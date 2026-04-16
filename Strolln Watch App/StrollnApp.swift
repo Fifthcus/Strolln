44224916372b2cf23b4452f348b53194f566148b
Michael <171223687+Bazorko@users.noreply.github.com>
2026-04-16 04:22:12 -0400

    Initial Commit

---
+++ Strolln Watch App/StrollnApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollnApp.swift
+//  Strolln Watch App
+//
+//  Created by Michael Stetter on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Strolln_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
